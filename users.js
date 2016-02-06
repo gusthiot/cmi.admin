27650a4f43ebad9e7f4084241a0d2d7340d426e5
Dominique Quatravaux <dominique@quatravaux.org>
2016-02-06 09:51:20 +0100

    Refactor search (and its bugs) into a library.
    * Move business code from lib/users.js to users.js
    * New file lib/search.js with a self-contained class; uses a ReactiveVar
      instead of a Session variable for the search state.

---
+++ users.js
@@ -0,0 +1,64 @@
+/**
+ * Model for users
+ */
+
+User = function(doc) {
+  _.extend(this, doc);
+};
+
+Users = new Mongo.Collection("users");
+
+UserSearch = new Search("user.search");
+
+if (Meteor.isServer) {
+
+  var ldapContext = Meteor.npmRequire('epfl-ldap')();
+  var getSyncUserByName = Meteor.wrapAsync(ldapContext.users.searchUserByName);
+
+  UserSearch.publish(function (query) {
+    var self = this;
+
+    if(query.length < 3) {
+      self.stop();
+      return;
+    }
+
+    if (Devsupport.isOnline()) {
+      getSyncUserByName(query).forEach(function (result) {
+        self.added("user.search.results", result.sciper, {link: "http://foo/bar", fullName: result.displayName});
+      });
+    }
+    Meteor.setTimeout(function () {
+      self.added("user.search.results", 3, {link: "http://foo/quux", fullName: "Last Guy"});
+      self.stop();
+    }, 1000);
+  });
+}
+
+if (Meteor.isClient) {
+  function boldifyMatchText(matchText, regExp) {
+      return matchText.replace(regExp, "<b>$&</b>");
+  }
+
+  Template.userSearchResult.helpers({
+    getUsers: _.bind(UserSearch.results.find, UserSearch.results, {}),
+    isLoading: _.bind(UserSearch.isLoading, UserSearch)
+  });
+
+  Template.userSearchBox.events({
+    "keyup #search-box": _.throttle(function(e) {
+      UserSearch.search($(e.target).val().trim());
+    }, 200)
+  });
+
+  Template.userLink.events({
+    "click a": function (e) {
+      $("#userdetails-" + this._id).toggle();
+      return false;
+    },
+    "submit form": function(e) {
+      console.log(this);
+      return false;
+    }
+  });
+}
