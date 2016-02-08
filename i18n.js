97a4a396ba627311206cb0f13dcfcd754bfa646c
Dominique Quatravaux <dominique@quatravaux.org>
2016-02-08 21:55:28 +0100

    Working, reactive language widget
    * Use a Tracker.autorun to set the menu both at load time and every time
      Session.get("i18nCurrentLanguage") changes
    * Works both ways: the widget changes the Session var and vice versa,
      yow! And the performance is excellent.

--- i18n.js
+++ i18n.js
@@ -4,15 +4,38 @@
 
 if (Meteor.isClient) {
   Session.set("i18nLanguages", [
-    {code: 'us', language: "English"},
+    {code: 'en', language: "English"},
     {code: 'fr', language: "Français"},
     {code: 'de', language: "Deutsch"},
     {code: 'it', language: "Italiano"}
   ]);
+  // TODO: Initialize from browser prefs; set from prefs of logged-in users
+  Session.setDefault("i18nCurrentLanguage", "fr");
   Template.i18nSelectLanguage.helpers({
-    languages: function() { return Session.get("i18nLanguages") }
+    languages: function() { return Session.get("i18nLanguages") },
   });
   Template.i18nSelectLanguage.onRendered(function () {
-    $(this.find(".selection.dropdown")).dropdown();
+    var langMenu = $(this.find(".selection.dropdown"));
+    // TODO: If no value yet, select one from Session.get("i18nCurrentLanguage")
+    langMenu.dropdown({
+      onChange: function (newValue) {
+        Session.set("i18nCurrentLanguage", newValue);
+      }
+    });
+    Tracker.autorun(function() {
+      langMenu.dropdown('set selected', Session.get("i18nCurrentLanguage"));
+    });
   });
+  Template.i18nPoorMansTest.helpers({
+    greeting: function() {
+      var lang = Session.get("i18nCurrentLanguage");
+      var greetings = {
+        fr: "Bonjour",
+        it: "Buon giorno",
+        en: "Hello",
+        de: "Guten Tag",
+      };
+      return greetings[lang];
+    }
+  })
 }
