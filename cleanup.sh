0e98aa777e1fd48db337ff7062c58aa4015dfea7
Dominique Quatravaux <dominique@quatravaux.org>
2016-02-18 17:42:58 +0100

    Add .npm cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -3,7 +3,8 @@
 # You wouldn't believe the time people waste trying to learn grunt,
 # isobuild and all that jazz. And no, we don't care all that much
 # about building on Windows.
-rm -rf .meteor/local/{build,bundler-cache,plugin-cache}/ \
+rm -rf .npm packages/*/.npm \
+  .meteor/local/{build,bundler-cache,plugin-cache,isopacks}/ \
   semantic/client/*.less semantic/client/.custom.semantic.json \
   semantic/client/{definitions,themes,site}
 
