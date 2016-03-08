09f07e1042106b29845fc1d2f84bae5c6abd0ef7
Dominique Quatravaux <dominique@quatravaux.org>
2016-03-08 17:50:59 +0100

    Selectable cells in /billables
    * Adopt "select" DataTable extension
    * On click events, figure out meaningful coords (i.e. column title
      and ID, instead of line and column nos)

--- billables/billables.js
+++ billables/billables.js
@@ -1,22 +1,38 @@
 Billables = new Meteor.Collection("billables");
 
-Billables.columns = function() {
-  var columns = [];
-  _.each(["type", "operatedByUser", "billableToAccount", "billableToProject",
-         "startTime", "billingDetails", "discount", "validationState"],
-        function(columnType) {
-          columns.push({
-            data: columnType,
-            title: "Billables.column." + columnType,
-            defaultContent: "-",
-            tmpl: Meteor.isClient && Template["Billable$cell$" + columnType]
-          });
-        });
-  return columns;
-};
+Billables.columns = _.map(
+  ["type", "operatedByUser", "billableToAccount", "billableToProject",
+   "startTime", "billingDetails", "discount", "validationState"],
+  function(columnType) {
+    return {
+      data: columnType,
+      title: "Billables.column." + columnType,  // i18n'd in lib/tabular.js
+      defaultContent: "-",
+      tmpl: Meteor.isClient && Template["Billable$cell$" + columnType]
+    };
+  });
 
 Billables.Table = new Tabular.Table({
   name: "Billables",
   collection: Billables,
-  columns: Billables.columns()
+  columns: Billables.columns,
+  // http://datatables.net/extensions/select/examples/initialisation/cells.html
+  select: {
+    style: "os",
+    items: "cell"
+  }
+});
+
+if (! Meteor.isClient) return; /******************************************/
+
+Template.Billables$Edit.onRendered(function() {
+  this.$("table").on('select.dt', function (e, dt, type, indexes) {
+    var rowNum = indexes[0].row, colNum = indexes[0].column,
+        data = dt.row(rowNum).data(),
+        columnDescr = Billables.columns[colNum];
+    console.log("Selected " + type + ": " + columnDescr.data + " of _id " + data._id);
+  })
+  .on('deselect.dt', function (e, dt, type, indexes) {
+    
+  });
 });
