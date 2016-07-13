3a191311fe4482ef9e3d2a9a63f5d1d75c752a45
the-dsw <fmod@the-dsw.com>
2016-07-13 11:23:34 +0200

    materialize and bootstrap

--- layout.js
+++ layout.js
@@ -6,76 +6,82 @@
  */
 
 Router.configure({
-  // the default layout that goes into <body>...</body>
-  layoutTemplate: "defaultLayout"
+    // the default layout that goes into <body>...</body>
+    layoutTemplate: "defaultLayout"
 });
 
 Router.route('/', function () {
-  this.render("Homepage");
+    this.render("Homepage");
 });
 
 renderUserSearchBoxInNavBar = function(thatRoute) {
-  thatRoute.render('User$Pick', {
-    to: "searchbox",
-    data: function() {
-      return {
-        withLDAP: true,
-        id: "LayoutUserSearch"
-      };
-    }
-  });
+    thatRoute.render('User$Pick', {
+        to: "searchbox",
+        data: function() {
+            return {
+                withLDAP: true,
+                id: "LayoutUserSearch"
+            };
+        }
+    });
 }
 
 if (Meteor.isClient) {
-  Template.User$Pick.events({
-    'user:selected #LayoutUserSearch': function(event, that, id) {
-      if (id === undefined) return;
-      var url = '/user/' + id + '/edit';
-      Router.go(url);
-    }
-  });
+    Template.User$Pick.events({
+        'user:selected #LayoutUserSearch': function(event, that, id) {
+            if (id === undefined) return;
+            var url = '/user/' + id + '/edit';
+            Router.go(url);
+        }
+    });
 }
 
 Router.route('/user', function () {
-  renderUserSearchBoxInNavBar(this);
+    renderUserSearchBoxInNavBar(this);
 });
 
 Router.route('/user/:sciper/edit', function () {
-  renderUserSearchBoxInNavBar(this);
-  var user = User.bySciper(this.params.sciper);
-  if (!user) {
-    this.render('AccessControl$PermissionDenied');
-  } else {
-    this.render('User$Edit', {data: {
-      object: user
-    }});
-  }
+    renderUserSearchBoxInNavBar(this);
+    var user = User.bySciper(this.params.sciper);
+    if (!user) {
+        this.render('AccessControl$PermissionDenied');
+    } else {
+        this.render('User$Edit', {data: {
+            object: user
+        }});
+    }
 });
 
 Router.route('/billables', function () {
-  this.render("Billables$Edit");
+    this.render("Billables$Edit");
 });
 
 if (Devsupport.isActive()) {
-  Router.route('/test', function () {
-    this.render("Test");
-  });
-  Router.route('/devsupport/kafka/(.*)', function () {
-    var topic = this.params[0];
-    Kafka.subscribe(topic);
-    this.render("Kafka", {data: {
-      topic: topic
-    }});
-  });
+    Router.route('/test', function () {
+        this.render("Test");
+    });
+    Router.route('/devsupport/kafka/(.*)', function () {
+        var topic = this.params[0];
+        Kafka.subscribe(topic);
+        this.render("Kafka", {data: {
+            topic: topic
+        }});
+    });
 }
 
 /* Work around some kind of URL mapping bug in bootstrap-3 */
 Router.route("/packages/bootstrap-3/(.*)",
-  function () {
-    this.response.writeHead(302, {
-      'Location': "/packages/mrt_bootstrap-3/" + this.params[0]
-    });
-    this.response.end();
-  },
-  { where: "server" });
+    function () {
+        this.response.writeHead(302, {
+            'Location': "/packages/mrt_bootstrap-3/" + this.params[0]
+        });
+        this.response.end();
+    },
+    { where: "server" });
 
+if (Meteor.isClient) {
+    Template.nav$burger.onRendered(function(){
+        var buttons = $(".button-collapse");
+        buttons.sideNav()
+    });
+}
