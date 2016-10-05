b43b4053d033b09cb15f5c9912dd759bec0ee70d
Dominique Quatravaux <dominique@quatravaux.org>
2016-10-05 20:58:20 +0200

    Straighten out Widget$Time and Widget$DateTime
    * Do data normalization to moment objects, like Widget$Date
    * Fix use case in billables.js (undoing f9f70691)
    * Use helper names in the same style as Widget$Date

--- billables/billables.js
+++ billables/billables.js
@@ -239,7 +239,7 @@ if (Meteor.isClient) {
                 operatedByUser: formData.operatedByUser.value(),
                 billableToAccount: formData.billableToAccount.value(),
                 billableToProject: formData.billableToProject.value(),
-                startTime: formData.startTime.value(),
+                startTime: formData.startTime.value().toDate(),
                 billingDetails: formData.billingDetails.value(),
                 discount: formData.discount.value(),
                 validationState: formData.validationState.value(),
